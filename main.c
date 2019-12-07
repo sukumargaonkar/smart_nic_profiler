e445c103d3303a8ac9ede1e3698553b65af0808d
Sukumar Gaonkar <sukumargaonkar@gmail.com>
2019-12-07 15:53:51 -0500

    Basic code for reading CTM and CLS memories

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <nfp.h>
+#include <stdlib.h>
+
+
+void cls_benchmark(){
+    __declspec(cls shared) int old[500];
+    __declspec(cls shared) int curr_time[8];
+    __declspec(cls shared) int time_handle[8];
+
+    int i, size;
+    
+    curr_time[__ctx()] = -1;
+    time_handle[__ctx()] = timestamp_start();
+
+    size = sizeof(old)/sizeof(int);
+    for(i = 1;i < 500; i++) {
+        old[i] = i;
+    }
+
+    curr_time[_ctx()] = timestamp_stop(time_handle[_ctx()]);
+}
+
+
+void ctm_benchmark(){
+    __declspec(ctm shared) int old[500];
+    __declspec(ctm shared) int curr_time[8];
+    __declspec(ctm shared) int time_handle[8];
+
+    int i, size;
+    
+    curr_time[__ctx()] = -1;
+    time_handle[__ctx()] = timestamp_start();
+
+    size = sizeof(old)/sizeof(int);
+    for(i = 1;i < 500; i++) {
+        old[i] = i;
+    }
+
+    curr_time[_ctx()] = timestamp_stop(time_handle[_ctx()]);
+}
+
+int main (){
+    ctm_benchmark();
+     cls_benchmark();
+    return 0;
+}
